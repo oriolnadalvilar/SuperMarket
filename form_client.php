6c20faf7e4e8119d424f1c186f9a1acc79b98c7c
Oriol Nadal <oriol.nadal@mataro.epiaedu.cat>
2021-04-15 17:08:20 +0200

    commit

--- form_client.php
+++ form_client.php
@@ -7,45 +7,72 @@
 					<div class="col-4 offset-2">
 						<div class="form-group">
 							<label for="username">Nom d'usuari (obligatori):</label>
-							<input type="text" class="form-control" name="username" id="username" />
+							<input required type="text" class="form-control" name="username" id="username" />
 						</div>
 						<div class="form-group">
 							<label for="pass">Contrasenya (obligatori):</label>
-							<input type="password" class="form-control" name="pass" id="pass" />
+							<input required type="password" class="form-control" name="pass" id="pass" />
 						</div>
 						<div class="form-group">
 							<label for="rp_pass">Repeteix la contrasenya (obligatori):</label>
-							<input type="password" class="form-control" name="rp_pass" id="rp_pass" />
+							<input required type="password" class="form-control" name="rp_pass" id="rp_pass" />
 						</div>
 						<div class="form-group">
 							<label for="nombre">Nom (obligatori):</label>
-							<input type="text" class="form-control" name="nombre" id="nombre" />
+							<input required type="text" class="form-control" name="nombre" id="nombre" />
 						</div>
 						<div class="form-group">
 							<label for="apellidos">Cognoms (obligatori):</label>
-							<input type="text" class="form-control" name="apellidos" id="apellidos" />
+							<input required type="text" class="form-control" name="apellidos" id="apellidos" />
 						</div>
 						<div class="form-group">
 							<label for="nif">NIF (obligatori):</label>
-							<input type="text" class="form-control" name="nif" id="nif" />
+							<input required type="text" class="form-control" name="nif" id="nif" />
 						</div>
 					</div>
 					<div class="col-4">
 						<div class="form-group">
 							<label for="direccion">Adreça (obligatori):</label>
-							<input type="text" class="form-control" name="direccion" id="direccion" />
+							<input required type="text" class="form-control" name="direccion" id="direccion" />
 						</div>
 						<div class="form-group">
 							<label for="codigo_postal">Codi postal (obligatori):</label>
-							<input type="text" class="form-control" name="codigo_postal" id="codigo_postal" />
+							<input required type="text" class="form-control" name="codigo_postal" id="codigo_postal" />
 						</div>
 						<div class="form-group">
 							<label for="poblacion">Població (obligatori):</label>
-							<select class="form-control" name="poblacion" id="poblacion">
+							<select required class="form-control" name="poblacion" id="poblacion">
 								<option value="">Selecciona una opció</option>
-								<option value="1">Badalona</option>
-								<option value="2">Barcelona</option>
-								<option value="3">Castelldefels</option>
+								<?php
+									include 'config.php';
+									$sql = "SELECT id_poblacio, nom FROM poblacions ORDER BY nom";
+									$conn = new mysqli($servername, $username, $password, $dbname);
+									$result = $conn->query($sql);
+
+									if ($result) {
+										if ($result->num_rows > 0) {
+											$row = $result->fetch_assoc();
+											while($row) {
+												$nom = $row["nom"];
+												$poblacio = $row["id_poblacio"];
+
+												echo "<option value=\"$poblacio\">$nom</option>";
+
+												$row = $result->fetch_assoc();
+											}
+
+											echo "</table>";
+
+
+											} else {
+												echo "<p>No hay ningún/a usuario/a</p>";
+											}
+
+										} else {
+											echo "ERROR al seleccionar los datos";
+										}
+									mysqli_close($conn);
+								?>
 							</select>
 						</div>
 						<div class="form-group">
@@ -63,5 +90,38 @@
 				</div>
 			</form>
 		</div>
+		<?php  
+			include 'config.php';
+			$conn = new mysqli($servername, $username, $password, $dbname);
+			
+			if (!empty($_POST)) {
+				$username = $_POST["username"];
+				$pass = $_POST["pass"];
+				$nombre = $_POST["nombre"];
+				$apellidos = $_POST["apellidos"];
+				$nif = $_POST["nif"];
+				$adreca = $_POST["direccion"];
+				$codigo_postal = $_POST["codigo_postal"];
+				$poblacion = $_POST["poblacion"];
+				$telefono = $_POST["telefono"];
+				$mail = $_POST["mail"];
+
+				$sql = "INSERT INTO clients (nom_usuari, contrasenya, nom, cognoms, nif, adreca, codi_postal, poblacio, telefon, email) VALUES ('$username', '$pass', '$nombre', '$apellidos', '$nif', '$adreca', '$codigo_postal', $poblacion, '$telefono', '$mail')";
+
+				//echo $sql;
+
+				$result = $conn->query($sql);
+
+				if ($result) {
+					echo '<script type="text/javascript">
+					           window.location = "entrar.php"
+					     </script>';
+				} 
+
+				
+
+			}
+			$conn->close();
+		?>
 	</body>
 </html>
